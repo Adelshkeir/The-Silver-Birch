89cf00a8ba1cac403b724c9384a20ede6cc1ad03
Adelshkeir <adelshkeir12@gmail.com>
2025-03-13 19:21:15 +0200

    finished the hero section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './hero.css'
+import React from "react";
+import "./hero.css";
 const Hero = () => {
   return (
-    <div className='Hero-Main'>
-      
+    <div className="Hero-Main">
+      <div className="Hero-Content">
+        <p className="Hero-Content-First">WINTER WARMTH</p>
+        <p className="Hero-Content-Second">
+          {" "}
+          Sent with love
+          <br />
+          from Maine
+        </p>
 
-aaaa
+        <a href="#" className="underline-animation">SHOP NOW</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
